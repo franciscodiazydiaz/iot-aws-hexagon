7d53eaf7d8c8b835659ab598fec1733a3d7c36a4
Francisco Diaz <fdiaz@edrans.com>
2019-01-23 14:02:42 -0300

    Add Prometheus and Grafana (docker-compose) Update .gitignore to ignore prometheus and grafana data dirs Updale src logic to expose Prometheus endpoint /metrics

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include "mgos_dht.h"
 #include "mgos_http_server.h"
 #include "mgos_dns_sd.h"
+#include "mgos_prometheus_metrics.h"
 
 static struct mgos_dht *s_dht = NULL;
 
@@ -20,10 +21,22 @@ static void root_handler(struct mg_connection *nc, int ev, void *p, void *user_d
 
   (void) p;
   if (ev != MG_EV_HTTP_REQUEST) return;
-  mg_send_response_line(nc, 200, "Content-Type: text/html\r\n");
+
+  mg_printf(nc, "HTTP/1.1 200 OK\r\n");
+  mg_send_response_line(nc, 200, "Content-Type: text/plain\r\n");
   mg_printf(nc, "Temperature: %f\r\n", t);
   mg_printf(nc, "Humidity: %f\r\n", h);
+
   nc->flags |= MG_F_SEND_AND_CLOSE;
+  (void) user_data;
+}
+
+static void prometheus_metrics_dht22(struct mg_connection *nc, void *user_data) {
+
+  mgos_prometheus_metrics_printf(nc, GAUGE, "temperature", "SysEng temperature in Celcius",
+                                 "%.2f", mgos_dht_get_temp(s_dht));
+  mgos_prometheus_metrics_printf(nc, GAUGE, "humidity", "SysEng humidity percentage",
+                                 "%.2f", mgos_dht_get_humidity(s_dht));
   (void) user_data;
 }
 
@@ -32,6 +45,7 @@ enum mgos_app_init_result mgos_app_init(void) {
   if ((s_dht = mgos_dht_create(23, DHT22)) == NULL) return MGOS_APP_INIT_ERROR;
 
   mgos_register_http_endpoint("/", root_handler, NULL);
+  mgos_prometheus_metrics_add_handler(prometheus_metrics_dht22, NULL);
 
   return MGOS_APP_INIT_SUCCESS;
 }
